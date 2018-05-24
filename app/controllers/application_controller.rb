8bb205c7350a47193347d855d641d9a32fb10bdb
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-24 14:49:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,7 @@ class ApplicationController < Sinatra::Base
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
+    @post.save
     erb :show
   end
 end
